4cf92ae68ef76422ed0f43bef804b670fa8f4740
athousandleaves <superamare@gmail.com>
2018-11-12 15:17:55 -0600

    Build out main component

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import SearchContainer from "./SearchContainer";
+import Results from "./Results";
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      searchResults: null,
+      details: null
+    };
+  }
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="main">
+        <SearchContainer search={this.search} loading={this.state.loading} />
+        <Results
+          searchResults={this.state.searchResults}
+          loading={this.state.loading}
+        />
+      </div>
+    );
   }
 }
